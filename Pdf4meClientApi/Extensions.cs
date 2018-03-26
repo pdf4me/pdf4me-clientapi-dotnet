a5f10d217852edbefd77ce26087ec6825d7af6c7
andreas.frei@ynoox.ch <andreas.frei@ynoox.ch>
2018-03-26 14:56:24 +0200

    added light method for optimize

--- Pdf4meClientApi/Extensions.cs
+++ Pdf4meClientApi/Extensions.cs
@@ -10,49 +10,49 @@ namespace Pdf4meClient
 {
     public static class Extension
     {
-        public static async Task<byte[]> OptimizeAsync(this LightClient pdfLightClient,  string profile, byte[] file, string fileName)
-        {
+        //public static async Task<byte[]> OptimizeAsync(this LightClient pdfLightClient,  string profile, byte[] file, string fileName)
+        //{
 
-            var message = new HttpRequestMessage();
-            var content = new MultipartFormDataContent();
+        //    var message = new HttpRequestMessage();
+        //    var content = new MultipartFormDataContent();
 
-            string controller = "PdfLight/Optimize";
-            string qparams = "profile=" + profile;
+        //    string controller = "Light/Optimize";
+        //    string qparams = "profile=" + profile;
 
-            string uriAddon = $"{controller}?{qparams}";
+        //    string uriAddon = $"{controller}?{qparams}";
 
-            using (var ms = new MemoryStream(file))
-            {
-                HttpClient client = Pdf4me.Instance.getApi();
+        //    using (var ms = new MemoryStream(file))
+        //    {
+        //        HttpClient client = Pdf4me.Instance.getApi();
                 
-                content.Add(new StreamContent(ms), "file", fileName);
+        //        content.Add(new StreamContent(ms), "file", fileName);
                 
-                message.Method = HttpMethod.Post;
-                message.Content = content;
-                message.RequestUri = new Uri($"{client.BaseAddress.AbsoluteUri}{uriAddon}");
+        //        message.Method = HttpMethod.Post;
+        //        message.Content = content;
+        //        message.RequestUri = new Uri($"{client.BaseAddress.AbsoluteUri}{uriAddon}");
 
 
-                var res = await client.SendAsync(message);
+        //        var res = await client.SendAsync(message);
 
-                //var optimizeRes = await res.Content.ReadAsAsync<OptimizeRes>();
-
-
-                var fileStr = await res.Content.ReadAsStreamAsync();
-
-                var msFile = new MemoryStream();
-                await fileStr.CopyToAsync(msFile);
-
-                var pobytes = msFile.ToArray();
-
-                return pobytes;
+        //        //var optimizeRes = await res.Content.ReadAsAsync<OptimizeRes>();
 
 
-                //return optimizeRes;
+        //        var fileStr = await res.Content.ReadAsStreamAsync();
 
-            }
+        //        var msFile = new MemoryStream();
+        //        await fileStr.CopyToAsync(msFile);
+
+        //        var pobytes = msFile.ToArray();
+
+        //        return pobytes;
+
+
+        //        //return optimizeRes;
+
+        //    }
             
 
-        }
+        //}
                
 
     }
--- Pdf4meClientApi/OptimizeClient.cs
+++ Pdf4meClientApi/OptimizeClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,52 @@ namespace Pdf4meClient
         {
             _pdfClient = pdfClient;
         }
+
+        public static async Task<byte[]> OptimizeAsync(OptimizeActionProfile profile, byte[] file, string fileName)
+        {
+
+            var message = new HttpRequestMessage();
+            var content = new MultipartFormDataContent();
+
+            string controller = "Light/Optimize";
+            string qparams = "profile=" + profile;
+
+            string uriAddon = $"{controller}?{qparams}";
+
+            using (var ms = new MemoryStream(file))
+            {
+                HttpClient client = Pdf4me.Instance.getApi();
+
+                content.Add(new StreamContent(ms), "file", fileName);
+
+                message.Method = HttpMethod.Post;
+                message.Content = content;
+                message.RequestUri = new Uri($"{client.BaseAddress.AbsoluteUri}{uriAddon}");
+
+
+                var res = await client.SendAsync(message);
+
+                //var optimizeRes = await res.Content.ReadAsAsync<OptimizeRes>();
+
+
+                var fileStr = await res.Content.ReadAsStreamAsync();
+
+                var msFile = new MemoryStream();
+                await fileStr.CopyToAsync(msFile);
+
+                var pobytes = msFile.ToArray();
+
+                return pobytes;
+
+
+                //return optimizeRes;
+
+            }
+
+
+        }
+
+
 
         public async Task<byte[]> OptimizeAsync( OptimizeActionProfile profile, byte[] document)
         {
