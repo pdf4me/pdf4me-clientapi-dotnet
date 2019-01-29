bbfd8cafb725dd9aab875eef53104bd3e6fee8d6
Varun Satish <varun.satish@ynoox.in>
2019-01-29 16:09:34 +0530

    updating assembly description

--- Pdf4meClientApi/Properties/AssemblyInfo.cs
+++ Pdf4meClientApi/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Pdf4me.Client")]
-[assembly: AssemblyDescription("The Pdf4me Client API is a .NET library which connects to its highly scalable SaaS cloud service with many functionalities to solve your document and PDF requirements. The SaaS api provides expert functionality to convert, optimize, compress, produce, merge, split, ocr, enrich, archive, print documents and PDFs.")]
+[assembly: AssemblyDescription("The Pdf4me Client API is a .NET library which connects to its highly scalable SaaS cloud service with many functionalities to solve your document and PDF requirements. The SaaS api provides expert functionality to convert, optimize, compress, produce, merge, split, ocr, enrich, archive, rotate, protect, validate, repair, print documents and PDFs.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pdf4me")]
 [assembly: AssemblyProduct("Pdf4meClientApi")]
